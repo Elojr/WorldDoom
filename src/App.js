80a15436f1bbeb4c7d77f65302300ba65198c30d
Otávio <otavio.pontes1103@gmail.com>
2020-04-21 01:28:14 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import Header from './components/Header/index';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
+      <Header />
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <h1>Corpo do App</h1>
     </>
   );
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  * {
+
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap');
+  
+* {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  
   }
+
+  header{
+    font: 700 17px Oswald, sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+
 `;
